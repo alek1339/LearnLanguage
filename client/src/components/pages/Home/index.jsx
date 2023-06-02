6a3bc6b2bb65dfee168151ef705aa64a2574ea89
alek1339 <aleksandermihaylov@gmail.com>
2023-06-02 16:33:21 +0300

    added localstorage for navigaion

--- client/src/components/pages/Home/index.jsx
+++ client/src/components/pages/Home/index.jsx
@@ -17,26 +17,50 @@ const Home = () => {
   const auth = useSelector((state) => state.auth);
   const profile = useSelector((state) => state.profile);
   const [level, setLevel] = useState("A1");
-  const [currentView, setCurrentView] = useState(LessonViews.All);
+  const [currentView, setCurrentView] = useState("");
   const [openedLesson, setOpenedLesson] = useState('');
 
   useEffect(() => {
+    const storedLevel = localStorage.getItem("level") || "A1";
+    const storedCurrentView = localStorage.getItem("currentView") || LessonViews.All;
+    const storedOpenedLesson = localStorage.getItem("openedLesson") || "";
+  
+    setLevel(storedLevel);
+    setCurrentView(storedCurrentView);
+    setOpenedLesson(storedOpenedLesson);
+  
     dispatch(fetchLessons());
     dispatch(fetchProfile({ id: auth.user.id }));
-  }, []);
+  }, [dispatch, auth.user.id]);
+
+  const handleSetLevel = (level) => {
+    localStorage.setItem("level", level);
+    setLevel(level);
+  }
+
+  const handleSetCurrentView = (view) => {
+    localStorage.setItem("currentView", view);
+    setCurrentView(view);
+  }
+
+  const handleSetCurrentOpenedLesson = (lesson) => {
+    localStorage.setItem("openedLesson", lesson);
+    setOpenedLesson(lesson);
+  }
+
 
   return (
     <div className="home-container">
       <Sidebar />
 
       <div className="main-content">
-        <LevelsNavbar level={level} setLevel={setLevel} setCurrentView={setCurrentView}/>
+        <LevelsNavbar level={level} setLevel={handleSetLevel} setCurrentView={handleSetCurrentView}/>
 
         <div className="lessons-view">
           {currentView === LessonViews.All && 
-            <Level lessons={lessons} level={level} profile={profile} setCurrentView={setCurrentView} setOpenedLesson={setOpenedLesson} />  
+            <Level lessons={lessons} level={level} profile={profile} setCurrentView={handleSetCurrentView} setOpenedLesson={handleSetCurrentOpenedLesson} />  
           }
-          {currentView === LessonViews.Lesson && <LessonPage openedLesson={openedLesson} setCurrentView={setCurrentView} />  }
+          {currentView === LessonViews.Lesson && <LessonPage openedLesson={openedLesson} setCurrentView={handleSetCurrentView} />  }
         </div>
       </div>
     </div>
